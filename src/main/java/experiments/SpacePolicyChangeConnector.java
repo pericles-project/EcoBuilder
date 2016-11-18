095d48047e4b14f3af078a29730877c5f267e8a5
Johannes <biermann@sub.uni-goettingen.de>
2016-11-18 09:52:04 +0100

    ERMR connection details and path now per command line arguments

--- src/main/java/experiments/SpacePolicyChangeConnector.java
+++ src/main/java/experiments/SpacePolicyChangeConnector.java
@@ -7,7 +7,12 @@ import java.io.*;
 
 
 /**
+ * This programm will output the space policy change demo model to a file
+ * and upload it to ERMR.
  * Created by Johannes on 15.11.16.
+ *
+ * Run it from command line with
+ * java -cp EcoBuilder-v1_1-jar-with-dependencies.jar experiments.SpacePolicyChangeConnector PATH ERMR_URL ERMR_REPOSIORY_NAME ERMR_USERNAME ERMR_PASSWWORD
  *
  */
 public class SpacePolicyChangeConnector {
@@ -17,11 +22,22 @@ public class SpacePolicyChangeConnector {
     private String namespace = "http://c102-086.cloud.gwdg.de/";
     // the path where the model shoud be saved
     private String path = "/tmp/";
+    private String ermrURL;
+    private String ermrRepository;
+    private String ermrUsername;
+    private String ermrPassword;
 
-    public SpacePolicyChangeConnector() {
+    public SpacePolicyChangeConnector(String path, String ermrURL, String ermrRepository, String ermrUsername, String ermrPassword) {
+        this.path = path;
+        this.ermrURL = ermrURL;
+        this.ermrRepository = ermrRepository;
+        this.ermrUsername = ermrUsername;
+        this.ermrPassword = ermrPassword;
+
         spaceExperiment = new SpacePolicyChangeExample();
 
         File turtleFile = new File(path+spaceExperiment.name+".ttl");
+        System.out.println("Saving model to " + turtleFile.getAbsolutePath());
         StringWriter outputWriter = new StringWriter();
         try {
             spaceExperiment.scenario.model.write(outputWriter, "TURTLE", namespace);
@@ -46,14 +62,14 @@ public class SpacePolicyChangeConnector {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        // send to ERMR Not tested yet
-        //sendToERMR(turtleFile);
+        System.out.println("Sending the model to ERMR.");
+        // send to ERMR
+        sendToERMR(turtleFile);
 
     }
 
     private void sendToERMR(File scenario) {
-        Configuration.setCredentials("MyRepository", "https://localhost", "user", "password");
+        Configuration.setCredentials(ermrRepository, ermrURL, ermrUsername, ermrPassword);
         ERMRConnection connection = new ERMRConnection();
         connection.send(scenario);
 
@@ -61,7 +77,18 @@ public class SpacePolicyChangeConnector {
 
 
     public static void main (String args[]) {
-        new SpacePolicyChangeConnector();
+        System.out.println("SpacePolicyChange Demo Helper. Writes the model to file and sends it to ERMR.");
+        if (null == args || args.length != 5) {
+            System.out.println("Error! no parameter given or wrong number of arguments.");
+            System.out.println("Usage: java experiments.SpacePolicyChangeConnector PATH ERMR_URL ERMR_REPOSIORY_NAME ERMR_USERNAME ERMR_PASSWWORD");
+            System.out.println("PATH is the path where the output file is saved - please add a trailing slash, ERMR_URL is the URL to ERMR, " +
+                    "ERMR_REPOSIORY is the name of the repository where the triples are stored, ERMR_USERNAME and ERMR_PASSWORD is a valid ERMR user.");
+            System.exit(1);
+        } else {
+            new SpacePolicyChangeConnector(args[0], args[1], args[2], args[3], args[4]);
+            System.out.println("Done.");
+            System.exit(0);
+        }
     }
 
 }
